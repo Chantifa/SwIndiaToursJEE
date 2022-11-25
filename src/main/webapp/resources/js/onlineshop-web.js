d4d56e16dec8b40512580fa950d51ac97ce6a2c1
chantifa <chantifa@windowslive.com>
2022-11-25 15:56:49 +0100

    link update frontend

--- src/main/webapp/resources/js/onlineshop-web.js
+++ src/main/webapp/resources/js/onlineshop-web.js
@@ -1,10 +1,10 @@
-$.get("navbar.html", function (data) {
+$.get("navbar.jsp", function (data) {
     $("#nav-placeholder").replaceWith(data);
     $.post("Navbar", function (data){
         $("#navbar-items").replaceWith(data);
     });
 });
 
-$.get("footer.html", function (data) {
+$.get("footer.jsp", function (data) {
     $("#footer-placeholder").replaceWith(data);
 });
